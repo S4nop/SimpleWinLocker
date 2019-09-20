bbae5a0f9ef948921465002708be4e5865cd5bb6
S4nop <rshtiger@naver.com>
2019-09-20 14:56:18 +0900

    Implemented

---
+++ frmLock.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WinLocker
+{
+    partial class FrmLock
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.lblLocked = new System.Windows.Forms.Label();
+            this.tmrKill = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtPW
+            // 
+            this.txtPW.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPW.Location = new System.Drawing.Point(659, 577);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.PasswordChar = '*';
+            this.txtPW.Size = new System.Drawing.Size(405, 53);
+            this.txtPW.TabIndex = 0;
+            this.txtPW.TextChanged += new System.EventHandler(this.TxtPW_TextChanged);
+            // 
+            // lblLocked
+            // 
+            this.lblLocked.AutoSize = true;
+            this.lblLocked.Font = new System.Drawing.Font("Forte", 222F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocked.ForeColor = System.Drawing.Color.Red;
+            this.lblLocked.Location = new System.Drawing.Point(411, 711);
+            this.lblLocked.Name = "lblLocked";
+            this.lblLocked.Size = new System.Drawing.Size(1815, 486);
+            this.lblLocked.TabIndex = 1;
+            this.lblLocked.Text = "LOCKED";
+            // 
+            // tmrKill
+            // 
+            this.tmrKill.Enabled = true;
+            this.tmrKill.Interval = 500;
+            this.tmrKill.Tick += new System.EventHandler(this.TmrKill_Tick);
+            // 
+            // FrmLock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(2544, 1429);
+            this.Controls.Add(this.lblLocked);
+            this.Controls.Add(this.txtPW);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLock";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmLock_FormClosing);
+            this.Load += new System.EventHandler(this.FrmLock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.Label lblLocked;
+        private System.Windows.Forms.Timer tmrKill;
+    }
+}
+
---
+++ frmLock.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace WinLocker
+{
+    public partial class FrmLock : Form
+    {
+        bool pw = false;
+        public FrmLock()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLock_Load(object sender, EventArgs e)
+        {
+            txtPW.Top = (this.Height - txtPW.Height + 400) / 2;
+            txtPW.Left = (this.Width - txtPW.Width) / 2;
+            lblLocked.Top = (this.Height - lblLocked.Height - 400) / 2;
+            lblLocked.Left = (this.Width - lblLocked.Width) / 2;
+        }
+
+        private void TmrKill_Tick(object sender, EventArgs e)
+        {
+            foreach(Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName.StartsWith("Taskmgr")) process.Kill();
+                if (process.ProcessName.StartsWith("explorer")) Process.Start("taskkill", "/F /IM explorer.exe");
+            }
+        }
+
+        private void TxtPW_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPW.Text.Equals("****"))
+            {
+                pw = true;
+                Process.Start("explorer");
+                Application.Exit();
+            }
+        }
+
+
+        private void FrmLock_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!pw) { e.Cancel = true; }
+        }
+    }
+}
